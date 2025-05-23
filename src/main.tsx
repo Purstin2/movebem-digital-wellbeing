e30fa7ef45c593f63356a67e3b097d91a1e0afa8
Purstin2 <MURILOFERRARE2022@GMAIL.COM>
2025-05-23 17:55:30 -0300

    okk

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App'
+import './styles/globals.css'; // Prioritize globals.css
+// import './index.css' // Temporarily comment out to avoid conflicts
 
 createRoot(document.getElementById("root")!).render(<App />);
